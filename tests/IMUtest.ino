8659857b3245eb1ec163b10932280fc88727a17d
nmk456 <nmk456@users.noreply.github.com>
2020-01-29 14:18:06 -0500

    Update IMUtest.ino

--- tests/IMUtest.ino
+++ tests/IMUtest.ino
@@ -8,18 +8,20 @@ LSM6DS3 imu;
 SPIFlash flash;
 struct Data {
   unsigned long time;
-  float aSubX;
-  float aSubY;
-  float aSubZ;
-  float omegaSubX;
-  float omegaSubY;
-  float omegaSubZ;
+  float ax;
+  float ay;
+  float az;
+  float gx;
+  float gy;
+  float gz;
 }
 Data thisData;
 File flightData;
 int address = 0;
 int maxAddress = 0;
+
 void saveToSD();
+
 void setup() {
   Serial.begin(9600);
   delay(1000);
@@ -29,33 +31,35 @@ void setup() {
   // Start the IMU
   imu.begin();
 }
+
 void loop() {
+  // Write values to struct, transfer to flash.
+  thisData = {millis(), myIMU.readFloatAccelX(), myIMU.readFloatAccelY(), myIMU.readFloatAccelZ()
+              myIMU.readFloatGyroX(), myIMU.readFloatGyroY(), myIMU.readFloatGyroZ()};
   // Time since launch
   Serial.print(F("Time since launch: "));
   Serial.println(millis());
   // Get all parameters from accelerometer.
   Serial.print(F("\nAccelerometer:\n"));
   Serial.print(F(" X = "));
-  Serial.println(myIMU.readFloatAccelX(), 4);
+  Serial.println(thisData.ax, 4);
   Serial.print(F(" Y = "));
-  Serial.println(myIMU.readFloatAccelY(), 4);
+  Serial.println(thisData.ay, 4);
   Serial.print(F(" Z = "));
-  Serial.println(myIMU.readFloatAccelZ(), 4);
+  Serial.println(thisData.az, 4);
   // Get all parameters from gyro.
   Serial.print("\nGyroscope:\n");
   Serial.print(F(" X = "));
-  Serial.println(myIMU.readFloatGyroX(), 4);
+  Serial.println(thisData.gx, 4);
   Serial.print(F(" Y = "));
-  Serial.println(myIMU.readFloatGyroY(), 4);
+  Serial.println(thisData.gy, 4);
   Serial.print(F(" Z = "));
-  Serial.println(myIMU.readFloatGyroZ(), 4);
-  // Write values to struct, transfer to flash.
-  thisData = {millis(), myIMU.readFloatAccelX(), myIMU.readFloatAccelY(), myIMU.readFloatAccelZ()
-                  myIMU.readFloatGyroX(), myIMU.readFloatGyroY(), myIMU.readFloatGyroZ()};
+  Serial.println(thisData.gz, 4);
   writeAnything(address, thisData);
   address += sizeOf(thisData);
   delay(100); //this will execute 10 times per second
 }
+
 void saveToSD() {
     int counter = 1;
     address = 0;
@@ -65,8 +69,7 @@ void saveToSD() {
     flightData = SD.open("data" + counter + ".csv");
     while( address < maxAddress ) {
         flash.readAnything(address, thisData);
-        flightData.println(thisData.aSubx, thisData.aSuby, thisData.aSubz, thisData.omegaSubx, thisData.omegaSuby,
-                           thisData.omegaSubZ);
+        flightData.println(thisData.ax, thisData.ay, thisData.az, thisData.gx, thisData.gy, thisData.gz);
         address += sizeof(thisData);
     }
 }
