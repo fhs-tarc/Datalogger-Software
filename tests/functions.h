f5a2874ae4482ac7665b52cfcc34157938ef2262
Eddie Hines <44607352+eddielife2k@users.noreply.github.com>
2020-01-31 15:04:59 -0500

    Updated drag function and added apogee function
    The drag function now incorporates density as a variable, which was previously missing. The apogee function can be found on the Math sheet https://docs.google.com/document/d/1emtXtxui2D5liDwP6ct_yjLZhZVrHU1mkUe4oe0dVFs/edit.

--- tests/functions.h
+++ tests/functions.h
@@ -1,9 +1,18 @@
-double drag(double v, double ce) {
-  double drag = v * v * ce;
+double drag(double v, double density) {
+  double a = 1.0;
+  double cdrag = 1.0;
+  double drag = ((v * v) * density) / 2) * cdrag * a;
   return drag;
 }
 double density(double pressure, double temperature) {
   double r = 6.02214 * pow(10,23);
   double m = 28.97;
   double density = (m * pressure) / (r * temperature);
+  return density;
+}
+double apogee(double ycurrent, double vcurrent, double theta, double pressure, double temperature) {
+  double g = 9.80665;
+  double density = density(pressure, temperature);
+  double apogee = (3 * vcurrent * vcurrent) / (2 * (g * drag(vcurrent, density) * cos(theta));
+  return apogee; 
 }
