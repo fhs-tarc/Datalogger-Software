533dd9479598e577906c5441807040621fa29979
Eddie Hines <44607352+eddielife2k@users.noreply.github.com>
2020-01-31 14:39:49 -0500

    Added the density function.
    This density takes data from our barometer and uses fundamental constants to calculate the density. The formula is derived from the ideal gas law.

--- tests/functions.h
+++ tests/functions.h
@@ -2,3 +2,8 @@ double drag(double v, double ce) {
   double drag = v * v * ce;
   return drag;
 }
+double density(double pressure, double temperature) {
+  double r = 6.02214 * pow(10,23);
+  double m = 28.97;
+  double density = (m * pressure) / (r * temperature);
+}
