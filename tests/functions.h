2c81d69f29da4ee9aafe482a5ad771aaf5f37fa1
nmk456 <nmk456@users.noreply.github.com>
2020-01-31 15:11:55 -0500

    Used processor macros, removed redundant statements

--- tests/functions.h
+++ tests/functions.h
@@ -1,18 +1,16 @@
-double drag(double v, double density) {
-  double a = 1.0;
-  double cdrag = 1.0;
-  double drag = ((v * v) * density) / 2) * cdrag * a;
-  return drag;
+#define R 6.02214 * pow(10,23)
+#define M 28.97
+#define g 9.80665
+
+double drag(double v, double density, double CdA) {
+  return drag = ((v * v) * density) / 2) * CdA;
 }
+
 double density(double pressure, double temperature) {
-  double r = 6.02214 * pow(10,23);
-  double m = 28.97;
-  double density = (m * pressure) / (r * temperature);
-  return density;
+  return density = (M * pressure) / (R * temperature);
 }
+
 double apogee(double ycurrent, double vcurrent, double theta, double pressure, double temperature) {
-  double g = 9.80665;
   double density = density(pressure, temperature);
-  double apogee = (3 * vcurrent * vcurrent) / (2 * (g * drag(vcurrent, density) * cos(theta));
-  return apogee; 
+  return apogee = (3 * vcurrent * vcurrent) / (2 * (g * drag(vcurrent, density) * cos(theta));
 }
