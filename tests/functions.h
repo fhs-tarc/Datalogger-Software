c2bc9f65bfa0a26aff0a9f8c599d58bfcbb134eb
Nathan Kerns <nmk456@gmail.com>
2020-01-31 18:25:00 -0500

    Fixed math errors

--- tests/functions.h
+++ tests/functions.h
@@ -1,6 +1,7 @@
 #define R 6.02214 * pow(10,23)
 #define M 28.97
 #define g 9.80665
+#define m 1 // mass in kg
 
 double drag(double v, double density, double CdA) {
   return CdA * v * v * density / 2;
@@ -12,5 +13,5 @@ double density(double pressure, double temperature) {
 
 double apogee(double ycurrent, double vcurrent, double theta, double pressure, double temperature, double CdA) {
   double current_density = density(pressure, temperature);
-  return ycurrent + (3 * vcurrent * vcurrent) / (2 * g * drag(vcurrent, current_density, CdA) * cos(theta));
+  return ycurrent + (3 * vcurrent * vcurrent) / (2 * (g + drag(vcurrent, current_density, CdA) * cos(theta)/m));
 }
