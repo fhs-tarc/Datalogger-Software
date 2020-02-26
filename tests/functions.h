5df354c31c0f24af5fdcc0c0f6fb8326cad2c8ce
Eddie Hines <44607352+eddielife2k@users.noreply.github.com>
2020-02-26 14:31:22 -0500

    Added getK and getDensity functions
    Created getK and getDensity functions to make our method of estimating the apogee work as intended.

--- tests/functions.h
+++ tests/functions.h
@@ -1,11 +1,13 @@
 #define g 9.80665
-
-// temperature in C, pressure in Pa, Cd, area in m2
-double get_k(double temperature, double pressure, double Cd, double A) {
-  return (pressure / ((temperature + 273.15) * 287.058)) * Cd * A / 2;
+// k is the wind coefficient 
+// temperature in C, pressure in Pa, Cd estimated experimentally, area in m2
+double getK(double density, double dragcoeff, double area) {
+  return density * dragcoeff * (area / 2);
 }
-
+double getDensity(double temp, double pressure) {
+  return pressure / ((temp + 273.15) * 287.058);
+}
 // current altitude in meters, current velocity in meters, wind resistance factor, mass in kg
-double apogee(double y, double v, double k, double m) {
+double getApogee(double ycurrente, double v, double k, double m) {
   return ycurrent + (m / (2 * k)) * log(1 + (k * v * v)/(m * g));
 }
